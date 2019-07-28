42358b2a2e4216a03d48af64099e1ccf7b57d311
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2019-07-28 15:52:17 +0100

    Removed Task.Run

--- TomLonghurst.RedisClient/Pipes/StreamPipe.cs
+++ TomLonghurst.RedisClient/Pipes/StreamPipe.cs
@@ -78,48 +78,45 @@ namespace TomLonghurst.RedisClient.Pipes
         public PipeReader Input =>
             _readPipe?.Reader ?? throw new InvalidOperationException("Cannot read from this pipe");
 
-        private void CopyFromStreamToReadPipe()
+        private async Task CopyFromStreamToReadPipe()
         {
             Exception exception = null;
             var writer = _readPipe.Writer;
 
-            Task.Run(new Action(async delegate
+            try
             {
-                try
+                while (true)
                 {
-                    while (true)
-                    {
-                        var memory = writer.GetMemory();
+                    var memory = writer.GetMemory();
 #if NETCORE
-                        var bytesRead = await _innerStream.ReadAsync(memory).ConfigureAwait(false);
+                    var bytesRead = await _innerStream.ReadAsync(memory).ConfigureAwait(false);
 #else
-                        var arr = memory.GetArraySegment();
+                    var arr = memory.GetArraySegment();
 
-                        var bytesRead = await _innerStream.ReadAsync(arr.Array, arr.Offset, arr.Count)
-                            .ConfigureAwait(false);
+                    var bytesRead = await _innerStream.ReadAsync(arr.Array, arr.Offset, arr.Count)
+                        .ConfigureAwait(false);
 #endif
 
-                        if (bytesRead == 0)
-                        {
-                            break;
-                        }
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
 
-                        writer.Advance(bytesRead);
+                    writer.Advance(bytesRead);
 
-                        var result = await writer.FlushAsync().ConfigureAwait(false);
-                        if (result.IsCompleted)
-                        {
-                            break;
-                        }
+                    var result = await writer.FlushAsync().ConfigureAwait(false);
+                    if (result.IsCompleted)
+                    {
+                        break;
                     }
                 }
-                catch (Exception e)
-                {
-                    exception = e;
-                }
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
 
-                writer.Complete(exception);
-            }));
+            writer.Complete(exception);
         }
 
         private long _totalBytesSent, _totalBytesReceived;
@@ -127,58 +124,55 @@ namespace TomLonghurst.RedisClient.Pipes
         //long IMeasuredDuplexPipe.TotalBytesSent => Interlocked.Read(ref _totalBytesSent);
         //long IMeasuredDuplexPipe.TotalBytesReceived => Interlocked.Read(ref _totalBytesReceived);
 
-        private void CopyFromWritePipeToStream()
+        private async Task CopyFromWritePipeToStream()
         {
             Exception exception = null;
             var reader = _writePipe.Reader;
-            
-            Task.Run(new Action(async delegate
-            {
-                try
-                {
-                    while (true)
-                    {
-                        var pendingReadResult = reader.ReadAsync();
-                        
-                        if (!pendingReadResult.IsCompleted)
-                        {
-                            await _innerStream.FlushAsync().ConfigureAwait(false);
-                        }
 
-                        var readResult = await pendingReadResult.ConfigureAwait(false);
-                        
-                        if (!readResult.Buffer.IsEmpty)
+            try
+            {
+                while (true)
+                {
+                    var pendingReadResult = reader.ReadAsync();
+
+                    if (!pendingReadResult.IsCompleted)
+                    {
+                        await _innerStream.FlushAsync().ConfigureAwait(false);
+                    }
+
+                    var readResult = await pendingReadResult.ConfigureAwait(false);
+
+                    if (!readResult.Buffer.IsEmpty)
+                    {
+                        foreach (var segment in readResult.Buffer)
                         {
-                            foreach (var segment in readResult.Buffer)
-                            {
 #if NETCORE
                             await _innerStream.WriteAsync(segment);
 #else
-                                var arraySegment = segment.GetArraySegment();
-                                await _innerStream
-                                    .WriteAsync(arraySegment.Array, arraySegment.Offset, arraySegment.Count)
-                                    .ConfigureAwait(false);
+                            var arraySegment = segment.GetArraySegment();
+                            await _innerStream
+                                .WriteAsync(arraySegment.Array, arraySegment.Offset, arraySegment.Count)
+                                .ConfigureAwait(false);
 #endif
-                            }
-
-                            await _innerStream.FlushAsync().ConfigureAwait(false);
                         }
 
-                        reader.AdvanceTo(readResult.Buffer.End);
+                        await _innerStream.FlushAsync().ConfigureAwait(false);
+                    }
 
-                        if (readResult.IsCompleted && readResult.Buffer.IsEmpty)
-                        {
-                            break;
-                        }
+                    reader.AdvanceTo(readResult.Buffer.End);
+
+                    if (readResult.IsCompleted && readResult.Buffer.IsEmpty)
+                    {
+                        break;
                     }
                 }
-                catch (Exception e)
-                {
-                    exception = e;
-                }
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
 
-                reader.Complete(exception);
-            }));
+            reader.Complete(exception);
         }
     }
 }
