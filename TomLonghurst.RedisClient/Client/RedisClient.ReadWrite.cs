ae80559ddb097740f27b199f533a5801ab208c3e
tom.longhurst <tom.longhurst@asos.com>
2019-09-17 13:30:33 +0100

    Optimise Imports

--- TomLonghurst.RedisClient/Client/RedisClient.ReadWrite.cs
+++ TomLonghurst.RedisClient/Client/RedisClient.ReadWrite.cs
@@ -1,18 +1,22 @@
 ﻿using System;
-using System.Buffers;
 using System.IO.Pipelines;
-using System.Linq;
 using System.Net.Sockets;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Threading.Tasks.Sources;
 using TomLonghurst.RedisClient.Exceptions;
 using TomLonghurst.RedisClient.Extensions;
 using TomLonghurst.RedisClient.Helpers;
 using TomLonghurst.RedisClient.Models;
 using TomLonghurst.RedisClient.Models.Backlog;
 using TomLonghurst.RedisClient.Models.Commands;
+
+#if NETSTANDARD
+using System.Linq;
+#endif
+
+#if NETCORE
+using System.Buffers;
+#endif
 
 namespace TomLonghurst.RedisClient.Client
 {
