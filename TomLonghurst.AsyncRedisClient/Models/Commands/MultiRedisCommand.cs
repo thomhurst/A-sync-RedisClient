b99b3d8f5df8b410bfeba3d9b9711775d1892ab1
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2019-09-18 21:32:11 +0100

    Implemented AsString for MultiRedisCommand

--- TomLonghurst.AsyncRedisClient/Models/Commands/MultiRedisCommand.cs
+++ TomLonghurst.AsyncRedisClient/Models/Commands/MultiRedisCommand.cs
@@ -1,12 +1,14 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using TomLonghurst.AsyncRedisClient.Extensions;
 
 namespace TomLonghurst.AsyncRedisClient.Models.Commands
 {
     public class MultiRedisCommand : IRedisCommand
     {
         public IList<byte[]> EncodedCommandList { get; }
-        public string AsString { get; }
+        public string AsString => string.Join("|", EncodedCommandList.Select(x => x.AsSpan().AsString()));
 
         public MultiRedisCommand(IEnumerable<IRedisCommand> redisCommands)
         {
