157cc4083752b1140c3b0a661978342c3b5d7672
tom.longhurst <tom.longhurst@asos.com>
2019-09-25 10:40:21 +0100

    Alpha 20

--- TomLonghurst.AsyncRedisClient/Exceptions/RedisFailedCommandException.cs
+++ TomLonghurst.AsyncRedisClient/Exceptions/RedisFailedCommandException.cs
@@ -2,7 +2,7 @@ using TomLonghurst.AsyncRedisClient.Models.Commands;
 
 namespace TomLonghurst.AsyncRedisClient.Exceptions
 {
-    public class RedisFailedCommandException : RedisException
+    public class RedisFailedCommandException : RedisRecoverableException
     {
         public RedisFailedCommandException(string message, IRedisCommand lastCommand)
         {
